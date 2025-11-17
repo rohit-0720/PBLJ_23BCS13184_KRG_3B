b6f97b75d347142bf8896ab33165ffdee6928753
Rohit Kumar <rohitmehta0720@gmail.com>
2025-11-17 18:24:56 +0530

    Create DBconnection.java

---
+++ External_Practical/DBconnection.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/banking_app";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Ayush11238@";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver not found.", e);
+        }
+    }
+}
